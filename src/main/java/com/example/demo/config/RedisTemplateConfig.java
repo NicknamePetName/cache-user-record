02ee0d6621a802afb3b30ba80709efef9dd278e0
Nickname <1013238728@qq.com>
2024-04-05 20:29:51 +0800

    nickname

--- src/main/java/com/example/demo/config/RedisTemplateConfig.java
+++ src/main/java/com/example/demo/config/RedisTemplateConfig.java
@@ -1,7 +1,5 @@
 package com.example.demo.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
